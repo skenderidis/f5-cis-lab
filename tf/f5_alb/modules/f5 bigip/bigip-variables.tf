66decbce1039dd9f1d1127844bd4b340d04c2b35
Kostas Skenderidis <skenderidis@gmail.com>
2021-09-18 08:21:07 +0400

    f5-alb configuration

---
+++ tf/f5_alb/modules/f5 bigip/bigip-variables.tf
@@ -0,0 +1,46 @@
+variable "azure_rg_name" {
+    type = string
+}
+variable "azure_region" {}
+
+#variable "AllowedIPs" {
+#  type = list(string)
+#}
+
+variable "f5_instance_type" {}
+variable "f5_image_name" {}
+variable "f5_version" {}
+
+variable "f5_product_name" {}
+
+variable libs_dir { 
+  default = "/config/cloud/azure/node_modules" 
+}
+variable onboard_log { 
+  default = "/var/log/startup-script.log" 
+}
+
+variable "prefix" {}
+variable "tag" {}
+variable "AS3_URL" {}
+variable "DO_URL" {}
+variable "TS_URL" {}
+variable "CFE_URL" {}
+variable "FAST_URL" {}
+variable "f5_password" {}
+variable "f5_username" {}
+
+variable "mgmt_subnet_id" {}
+variable "int_subnet_id" {}
+variable "ext_subnet_id" {}
+variable "mgmt_nsg_id" {}
+variable "ext_nsg_id" {}
+variable "self_ip_mgmt" { }
+variable "self_ip_ext" { }
+variable "self_ip_int" { }
+variable "add_ip_ext_1" { }
+variable "add_ip_ext_2" { }
+variable "add_ip_ext_3" { }
+variable "pip_ext_1_id" { }
+variable "pip_ext_2_id" { }
+variable "pip_ext_3_id" { }

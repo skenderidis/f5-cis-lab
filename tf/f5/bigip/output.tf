c36af2ad50c1f09f2ad14af0259eaf9854bf82a8
Kostas Skenderidis <skenderidis@gmail.com>
2021-12-15 09:27:14 +0400

    Update output.tf

--- tf/f5/bigip/output.tf
+++ tf/f5/bigip/output.tf
@@ -9,6 +9,15 @@ output "name" {
 output "app1_public_ip" {
   value = azurerm_public_ip.pip_app1.ip_address
 }
+output "app2_public_ip" {
+  value = azurerm_public_ip.pip_app2.ip_address
+}
+output "app3_public_ip" {
+  value = azurerm_public_ip.pip_app3.ip_address
+}
+output "app4_public_ip" {
+  value = azurerm_public_ip.pip_app4.ip_address
+}
 
 
 output "id" {
